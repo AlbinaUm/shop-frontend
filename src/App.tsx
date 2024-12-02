7bd6c2f124d0ee224dd24091dbd8a8d9e570af36
Umarova Albina <albinaumarova2014@gmail.com>
2024-12-02 18:02:44 +0600

    Init app

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { Container, CssBaseline } from "@mui/material";
+
+const App = () => {
+  return (
+    <>
+      <CssBaseline />
+      <header>
+        {/*// туллбар будет тут*/}
+      </header>
+
+      <main>
+        <Container maxWidth="xl">
+          {/*роуты будут тут*/}
+        </Container>
+      </main>
+    </>
+  );
+};
+
+export default App;
