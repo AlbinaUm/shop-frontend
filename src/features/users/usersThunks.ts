a1dc2fa8a185ebd2baa7be6dbf8995091044dfbd
Umarova Albina <albinaumarova2014@gmail.com>
2025-01-16 17:00:38 +0600

    [frontend] Added register logic

---
+++ src/features/users/usersThunks.ts
@@ -0,0 +1,25 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import {RegisterMutation, RegisterResponse, ValidationError} from '../../types';
+import axiosApi from '../../axiosApi.ts';
+import {isAxiosError} from "axios";
+
+
+export const register = createAsyncThunk<
+    RegisterResponse,
+    RegisterMutation,
+    {rejectValue: ValidationError}
+>(
+  'users/register',
+  async (registerMutation: RegisterMutation, {rejectWithValue}) => {
+      try {
+          const response = await axiosApi.post<RegisterResponse>('/users/register', registerMutation);
+          return response.data;
+      } catch (error) {
+         if (isAxiosError(error) && error.response && error.response.status === 400) {
+             return rejectWithValue(error.response.data);
+         }
+
+          throw error;
+      }
+  }
+)
