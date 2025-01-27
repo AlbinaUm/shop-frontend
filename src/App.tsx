c98e9050093629260eddc0b21eb4bb29efdbf507
Umarova Albina <albinaumarova2014@gmail.com>
2025-01-28 01:46:39 +0600

    [frontend] Added interceptor to axiosAPI. Create component ProtectedRoute. Close route products/new.

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,12 @@ import Products from "./features/products/containers/Products.tsx";
 import NewProduct from "./features/products/containers/NewProduct.tsx";
 import RegisterPage from './features/users/RegisterPage.tsx';
 import LoginPage from './features/users/LoginPage.tsx';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute.tsx';
+import { useAppSelector } from './app/hooks.ts';
+import { selectUser } from './features/users/usersSlice.ts';
 
 const App = () => {
+  const user = useAppSelector(selectUser);
   return (
     <>
       <CssBaseline />
@@ -21,7 +25,13 @@ const App = () => {
             <Route path="/register" element={<RegisterPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/products" element={<Products />} />
-            <Route path="/products/new" element={<NewProduct />} />
+
+            <Route path="/products/new" element={
+              <ProtectedRoute isAllowed={user && user.role === 'admin'}>
+                <NewProduct />
+              </ProtectedRoute>
+            } />
+
             <Route path="*" element={<h1>Not found</h1>} />
           </Routes>
         </Container>
